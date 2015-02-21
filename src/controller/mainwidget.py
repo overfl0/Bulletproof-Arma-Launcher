4289a71ca35914015b63e659d94c89a9479d6be8
Sighter <sighter@resource-dnb.de>
2015-02-21 16:31:41 +0100

    added working multiprocessing example

--- src/controller/mainwidget.py
+++ src/controller/mainwidget.py
@@ -1,4 +1,36 @@
+from multiprocessing import Pool
+
 import kivy
+import requests
+import os
+
+from time import sleep
+
+def download(path):
+    print "iam process:"
+    print "downloading to:", path
+    sleep(10)
+    # with open(os.path.join(path, 'kivy.zip'), 'wb') as handle:
+    #
+    #     print "get request"
+    #     response = requests.get(
+    #         'http://kivy.org/downloads/1.8.0/Kivy-1.8.0-py2.7-win32.zip',
+    #         stream=True
+    #     )
+    #
+    #     print "request ready"
+    #
+    #     if not response.ok:
+    #         print 'response failed'
+    #
+    #     for block in response.iter_content(1024):
+    #         if not block:
+    #             break
+    #
+    #         handle.write(block)
+
+def on_download_finish(*args, **kwargs):
+    print "download finished", args, kwargs
 
 class MainWidgetController(object):
     def __init__(self, widget):
@@ -10,3 +42,10 @@ class MainWidgetController(object):
         print 'button clicked', str(btn), str(image)
         image.source = app.resource_path('images/installing.png')
         print 'MainWidget ids:', self.view.ids
+        self.test_file_download()
+
+    def test_file_download(self):
+        pool = Pool(processes=2)
+        pool.apply_async(download, (os.getcwd(),), callback=on_download_finish)
+        # pool.close()
+        # pool.join()
--- src/launcher.py
+++ src/launcher.py
@@ -1,51 +1,53 @@
+
 import sys
 import os
 
-from kivy.config import Config
-Config.set('graphics','resizable',0)
-Config.set('graphics', 'width', '1000')
-Config.set('graphics', 'height', '666')
-Config.set('graphics','borderless',1)
-import kivy
-kivy.require('1.8.0') # replace with your current kivy version !
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.widget import Widget
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.stacklayout import StackLayout
-from kivy.graphics import Line, Color
-from kivy.properties import NumericProperty, ObjectProperty, BooleanProperty, StringProperty
-from kivy.core.window import Window
-from kivy.clock import Clock
+if __name__ == "__main__":
+    from kivy.config import Config
+    Config.set('graphics','resizable',0)
+    Config.set('graphics', 'width', '1000')
+    Config.set('graphics', 'height', '666')
+    Config.set('graphics','borderless',1)
+    import kivy
+    kivy.require('1.8.0') # replace with your current kivy version !
+    from kivy.app import App
+    from kivy.uix.label import Label
+    from kivy.uix.widget import Widget
+    from kivy.uix.boxlayout import BoxLayout
+    from kivy.uix.button import Button
+    from kivy.uix.stacklayout import StackLayout
+    from kivy.graphics import Line, Color
+    from kivy.properties import NumericProperty, ObjectProperty, BooleanProperty, StringProperty
+    from kivy.core.window import Window
+    from kivy.clock import Clock
 
-from view.hoverbutton import HoverButton
-from controller.mainwidget import MainWidgetController
+    from view.hoverbutton import HoverButton
+    from controller.mainwidget import MainWidgetController
 
 
-class MainWidget(Widget):
-    """
-    root widget of the app
-    """
-    def __init__(self, **kwargs):
-        super(MainWidget, self).__init__(**kwargs)
-        self.controller = MainWidgetController(self)
-
-class LauncherApp(App):
-
-    def build(self):
-
-        # basic
-        return MainWidget()
-
-    def resource_path(self, relative):
+    class MainWidget(Widget):
         """
-        This method makes sure that the app can access resource path
-        also if packed within a single executable
+        root widget of the app
         """
-        if hasattr(sys, "_MEIPASS"):
-            return os.path.join(sys._MEIPASS, relative)
-        return os.path.join('../resources', relative)
+        def __init__(self, **kwargs):
+            super(MainWidget, self).__init__(**kwargs)
+            self.controller = MainWidgetController(self)
 
-if __name__ == '__main__':
-    launcher_app = LauncherApp().run()
+    class LauncherApp(App):
+
+        def build(self):
+
+            # basic
+            return MainWidget()
+
+        def resource_path(self, relative):
+            """
+            This method makes sure that the app can access resource path
+            also if packed within a single executable
+            """
+            if hasattr(sys, "_MEIPASS"):
+                return os.path.join(sys._MEIPASS, relative)
+            return os.path.join('../resources', relative)
+
+    if __name__ == '__main__':
+        launcher_app = LauncherApp().run()
