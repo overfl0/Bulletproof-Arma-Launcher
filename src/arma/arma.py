98b7881939a642487144231d67eb2cb799e4066d
Lukasz Taczuk <overfl0@users.noreply.github.com>
2015-03-08 23:20:45 +0100

    Custom path for Arma class. Singleton metaclass.

--- src/arma/arma.py
+++ src/arma/arma.py
@@ -1,26 +1,50 @@
-import os
+# import Windows registry package while ensuring cygwin compatibility
 try:
-    import cygwinreg as _winreg  # Cygwin compatibility
+    import cygwinreg as _winreg
     WindowsErrorPortable = _winreg.WindowsError
 except ImportError:
     import _winreg
     WindowsErrorPortable = WindowsError
 
+import os
+import sys
 
+# Allow relative imports when the script is run from the command line
+if __name__ == "__main__":
+    sys.path.append(os.path.dirname(sys.path[0]))
+
+from utils.singleton import Singleton
+
+
+# Exceptions:
 class ArmaNotInstalled(Exception):
     pass
 
 
-class Arma:
+class Arma(object):
+    __metaclass__ = Singleton
+    __custom_path = None
 
     # Registry paths
-    _arma_registry_path = "software\\bohemia interactive\\arma 3"
-    _arma_expansions_registry_path = "software\\bohemia interactive\\arma 3\\expansions\\arma 3"
+    _arma_registry_path = r"software\bohemia interactive\arma 3"
+    _arma_expansions_registry_path = r"software\bohemia interactive\arma 3\expansions\arma 3"
 
     @staticmethod
     def get_user_path():
         """Returns the place where mods can be installed in the user folder."""
-        return os.path.expanduser('~')  # TODO: Check me when run as administrator
+        return os.path.expanduser('~')
+
+    @staticmethod
+    def get_custom_path():
+        """Returns a custom mod installation path set by the user.
+        If no path has been set beforehand, returns None"""
+        return Arma().__custom_path
+
+    @staticmethod
+    def set_custom_path(new_path):
+        """Set a custom mod installation path.
+        Note: The function does NOT convert the path to an absolute path."""
+        Arma().__custom_path = new_path
 
     @staticmethod
     def get_installation_path():
@@ -52,5 +76,21 @@ class Arma:
         pass  # Stub
 
 if __name__ == "__main__":
-    print Arma.get_installation_path()
-    print Arma.get_executable_path()
+    a = Arma()
+    b = Arma()
+    c = Arma()
+    Arma.set_custom_path("asd")
+    print a.get_custom_path()
+    print b.get_custom_path()
+    print c.get_custom_path()
+    print Arma.get_custom_path()
+
+    b.set_custom_path("bsd")
+    print a.get_custom_path()
+    print b.get_custom_path()
+    print c.get_custom_path()
+    print Arma.get_custom_path()
+
+
+    #print Arma.get_executable_path()
+    pass
