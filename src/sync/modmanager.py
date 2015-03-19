1614dec1ebf1feed77b12eed46326127b9b4c09f
Sighter <sighter@resource-dnb.de>
2015-03-19 02:39:37 +0100

    fix git fail

--- src/sync/modmanager.py
+++ src/sync/modmanager.py
@@ -30,8 +30,9 @@ def get_mod_descriptions(messagequeue):
     to pass in a queue
     """
     url = 'https://gist.githubusercontent.com/Sighter/adbce21192d0413cfbad/raw/074c5227b54ca7cb2abce918f08ce4b6a8362f66/moddesc.json'
-    res = requests.get(url)
+    res = requests.get(url, verify=False)
     data = None
+
     mods = []
 
     if res.status_code != 200:
@@ -49,40 +50,10 @@ def get_mod_descriptions(messagequeue):
 
     return mods
 
-class SubProcess(Process):
-    # TODO think about removing this
-    def __init__(self, syncclass, resultQueue, mod):
-        self.resultQueue = resultQueue
-        self.syncclass = syncclass
-        self.mod = mod
-
-        multiprocessing.Process.__init__(self)
-        self.start()
-
-    def run(self):
-        syncer = self.syncclass(self.resultQueue, self.mod)
-        syncer.sync()
-
 class ModManager(object):
     """docstring for ModManager"""
     def __init__(self):
         super(ModManager, self).__init__()
-
-    def _sync_single_mod(self, mod):
-
-        # TODO do not thread of here
-
-        loc = mod.clientlocation
-        url = mod.downloadurl
-        syncclass = None
-
-        if mod.synctype == 'http':
-            syncclass = HttpSyncer
-
-        Logger.debug('ModManager: syncing mod:' + mod.name)
-
-        self.current_queue = Queue()
-        SubProcess(syncclass, self.current_queue, mod);
 
     def _check_already_installed_with_six(self, mod):
         """returns true if mod is installed already with withsix, otherwise false"""
@@ -100,16 +71,7 @@ class ModManager(object):
 
         return os.path.isfile(mod_path)
 
-    def _get_syncer(self, type):
-        """
-        gets a syncer CLASS by type
-        """
-        if type == 'http':
-            return HttpSyncer
-
-        return None
-
-    def sync_all(self, messagequeue):
+    def prepare_and_check(self, messagequeue):
         """do everything which is needed to get all mods in sync"""
 
         # download mod descriptions first
@@ -131,16 +93,22 @@ class ModManager(object):
             'action': 'checkmods', 'status': 'finished',
             'progress': 0.1, 'kbpersec': 0.0})
 
+
+    def sync_all(self, messagequeue):
+
         # TODO: Sync via libtorrent
+        # The following is just test code
+
+        mod = Mod(
+            name='@CBA_A3',
+            clientlocation='../tests/',
+            synctype='http',
+            downloadurl='http://dev.withsix.com/attachments/download/22231/CBA_A3_RC4.7z');
+
+        syncer = HttpSyncer(messagequeue, mod)
+        syncer.sync()
 
         return
-
-    def query_status(self):
-        if not self.current_queue.empty():
-            progress = self.current_queue.get_nowait()
-            return progress
-        else:
-            return None
 
 if __name__ == '__main__':
 
