2471ca9d5ed9ee2c01f994c6839cd94b17ce3043
Sighter <sighter@resource-dnb.de>
2015-07-08 00:57:53 +0200

    change alpha version

--- src/gui/installscreen.py
+++ src/gui/installscreen.py
@@ -96,7 +96,7 @@ class Controller(object):
 
     def update_footer_label(self, dt):
         git_sha1 = get_git_sha1_auto()
-        version = 'Alpha 1'
+        version = 'Alpha 2'
         footer_text = '{}\nBuild: {}'.format(version,
                                              git_sha1[:7] if git_sha1 else 'N/A')
         self.view.ids.footer_label.text = footer_text
