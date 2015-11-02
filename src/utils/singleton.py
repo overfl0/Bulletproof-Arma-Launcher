20f546bebb256d36ecf70bd0fa12f9be9e25bdd1
Lukasz Taczuk <overfl0@users.noreply.github.com>
2015-11-02 23:33:33 +0100

    Use unicode by default. See #66.

--- src/utils/singleton.py
+++ src/utils/singleton.py
@@ -10,6 +10,8 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
+from __future__ import unicode_literals
+
 class Singleton(type):
     _instances = {}
     def __call__(cls, *args, **kwargs):
