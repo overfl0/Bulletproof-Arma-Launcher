5cb022a658f9f51a3904ac8240c8403952276b6c
Lukasz Taczuk <overfl0@users.noreply.github.com>
2015-11-03 00:38:30 +0100

    Handle any unhandled exception. Resolves #70.

--- src/launcher.py
+++ src/launcher.py
@@ -12,113 +12,131 @@
 
 from __future__ import unicode_literals
 
-# Enforce all requirements so that the program doesn't crash in the middle of execution.
-if __name__ == '__main__':
-    from utils.requirements import check_libraries_requirements
-    check_libraries_requirements()
+# Mega catch-all. This is ugly but probably the only way to show users a message
+# in any possible case something fails at any possible place.
+try:
 
-# Import multiprocessing and enable freeze_support which is needed on windows
-import multiprocessing
-multiprocessing.freeze_support()
-
-# Import kivy as soon as possible to let it eat all the kivy args from sys.argv
-import kivy
-
-#
-# we have to protect the instantiation of the kivy app, cause
-# of the use of multiprocessing. If you spawn a new thread or process
-# it loads this file again. So there is the need of the __main__ guard.
-#
-if __name__ == "__main__":
-    import sys
-
-    # initilize settings class
-    from utils.settings import Settings
-    settings = Settings(sys.argv[1:])
-
-    # HACK: clear sys.argv for kivy. Keep only the first element
-    #sys.argv = sys.argv[0:1]
-
-    # configure kivy
-    from kivy.config import Config
-
-    if not settings.get('self_update'):
-        Config.set('graphics','resizable',0)
-        Config.set('graphics', 'width', 1000)
-        Config.set('graphics', 'height', 666)
-        Config.set('graphics','borderless',1)
-    else:
-        Config.set('graphics','resizable',0)
-        Config.set('graphics', 'width', 400)
-        Config.set('graphics', 'height', 150)
-        Config.set('graphics','borderless',1)
-
-    #
-    # other imports
-    #
-    from kivy.app import App
-    from kivy.uix.label import Label
-    from kivy.uix.widget import Widget
-    from kivy.uix.boxlayout import BoxLayout
-    from kivy.uix.button import Button
-    from kivy.uix.stacklayout import StackLayout
-    from kivy.graphics import Line, Color
-    from kivy.properties import NumericProperty, ObjectProperty, BooleanProperty, StringProperty
-    from kivy.core.window import Window
-    from kivy.clock import Clock
-    from kivy.logger import Logger
-    from kivy.uix.screenmanager import ScreenManager, Screen
-    from kivy.core.text import LabelBase
-    from kivy.base import ExceptionManager
-
-    from utils.app import BaseApp
-    from view.hoverbutton import HoverButton
-    from view.statusimage import StatusImage
-    from view.errorpopup import error_popup_decorator
-    from view.errorpopup import PopupHandler
-
-    from gui.mainwidget import MainWidget
-    from gui.updatermainwidget import UpdaterMainWidget
-    from gui.installscreen import InstallScreen
-    from gui.prefscreen import PrefScreen
-
-    import logging
-
-    if settings.get('use_exception_popup') == True:
-        ExceptionManager.add_handler(PopupHandler())
-
-    class MainScreenManager(ScreenManager):
-        pass
-
-    class LauncherApp(BaseApp):
-        """Main class for the normal app"""
-        def __init__(self, settings):
-            super(LauncherApp, self).__init__()
-            self.settings = settings
-
-        def build(self):
-            logger = logging.getLogger('concurrent.futures')
-            logger.addHandler(logging.StreamHandler())
-            return MainWidget()
-
-    class SelfUpdaterApp(BaseApp):
-        """app which starts the self updater"""
-
-        def __init__(self, settings):
-            super(SelfUpdaterApp, self).__init__()
-
-        def build(self):
-            logger = logging.getLogger('concurrent.futures')
-            logger.addHandler(logging.StreamHandler())
-            return UpdaterMainWidget()
-
+    # Enforce all requirements so that the program doesn't crash in the middle of execution.
     if __name__ == '__main__':
-        launcher_app = None
+        from utils.requirements import check_libraries_requirements
+        check_libraries_requirements()
 
-        if settings.get('self_update'):
-            print 'launching self updater'
-            launcher_app = SelfUpdaterApp(settings).run()
+    # Import multiprocessing and enable freeze_support which is needed on windows
+    import multiprocessing
+    multiprocessing.freeze_support()
+
+    # Import kivy as soon as possible to let it eat all the kivy args from sys.argv
+    import kivy
+
+    #
+    # we have to protect the instantiation of the kivy app, cause
+    # of the use of multiprocessing. If you spawn a new thread or process
+    # it loads this file again. So there is the need of the __main__ guard.
+    #
+    if __name__ == "__main__":
+        import sys
+
+        # initilize settings class
+        from utils.settings import Settings
+        settings = Settings(sys.argv[1:])
+
+        # HACK: clear sys.argv for kivy. Keep only the first element
+        # sys.argv = sys.argv[0:1]
+
+        # configure kivy
+        from kivy.config import Config
+
+        if not settings.get('self_update'):
+            Config.set('graphics', 'resizable', 0)
+            Config.set('graphics', 'width', 1000)
+            Config.set('graphics', 'height', 666)
+            Config.set('graphics', 'borderless', 1)
         else:
-            launcher_app = LauncherApp(settings)
-            launcher_app.run = error_popup_decorator(launcher_app.run)
-            launcher_app.run()
+            Config.set('graphics', 'resizable', 0)
+            Config.set('graphics', 'width', 400)
+            Config.set('graphics', 'height', 150)
+            Config.set('graphics', 'borderless', 1)
+
+        #
+        # other imports
+        #
+        from kivy.app import App
+        from kivy.uix.label import Label
+        from kivy.uix.widget import Widget
+        from kivy.uix.boxlayout import BoxLayout
+        from kivy.uix.button import Button
+        from kivy.uix.stacklayout import StackLayout
+        from kivy.graphics import Line, Color
+        from kivy.properties import NumericProperty, ObjectProperty, BooleanProperty, StringProperty
+        from kivy.core.window import Window
+        from kivy.clock import Clock
+        from kivy.logger import Logger
+        from kivy.uix.screenmanager import ScreenManager, Screen
+        from kivy.core.text import LabelBase
+        from kivy.base import ExceptionManager
+
+        from utils.app import BaseApp
+        from view.hoverbutton import HoverButton
+        from view.statusimage import StatusImage
+        from view.errorpopup import error_popup_decorator
+        from view.errorpopup import PopupHandler
+
+        from gui.mainwidget import MainWidget
+        from gui.updatermainwidget import UpdaterMainWidget
+        from gui.installscreen import InstallScreen
+        from gui.prefscreen import PrefScreen
+
+        import logging
+
+        if settings.get('use_exception_popup') == True:
+            ExceptionManager.add_handler(PopupHandler())
+
+        class MainScreenManager(ScreenManager):
+            pass
+
+        class LauncherApp(BaseApp):
+            """Main class for the normal app"""
+            def __init__(self, settings):
+                super(LauncherApp, self).__init__()
+                self.settings = settings
+
+            def build(self):
+                logger = logging.getLogger('concurrent.futures')
+                logger.addHandler(logging.StreamHandler())
+                return MainWidget()
+
+        class SelfUpdaterApp(BaseApp):
+            """app which starts the self updater"""
+
+            def __init__(self, settings):
+                super(SelfUpdaterApp, self).__init__()
+
+            def build(self):
+                logger = logging.getLogger('concurrent.futures')
+                logger.addHandler(logging.StreamHandler())
+                return UpdaterMainWidget()
+
+        if __name__ == '__main__':
+            launcher_app = None
+
+            if settings.get('self_update'):
+                print 'launching self updater'
+                launcher_app = SelfUpdaterApp(settings).run()
+            else:
+                launcher_app = LauncherApp(settings)
+                launcher_app.run = error_popup_decorator(launcher_app.run)
+                launcher_app.run()
+
+except Exception as e:
+    # Mega catch-all requirements
+    import traceback
+
+    from utils.critical_messagebox import MessageBox
+    from utils.primitive_git import get_git_sha1_auto
+
+    CRITICAL_POPUP_TITLE = """An error occurred. Copy it with Ctrl+C and submit a bug"""
+    build = get_git_sha1_auto()
+    stacktrace = "".join(traceback.format_exception(*sys.exc_info()))
+    msg = 'Build: {}\n{}'.format(build, stacktrace)
+
+    MessageBox(msg, CRITICAL_POPUP_TITLE)
