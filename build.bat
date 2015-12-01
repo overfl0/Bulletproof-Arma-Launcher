effe57c7ae7c4d446273265c1bf0a5632b82eb7f
Lukasz Taczuk <overfl0@users.noreply.github.com>
2015-12-01 01:29:35 +0100

    Fixed command to run unit tests. Disable unit tests for now.

--- build.bat
+++ build.bat
@@ -1,10 +1,10 @@
-@echo #########################################################################
-@echo Running unit tests...
-@echo #########################################################################
-nosetests tests -a --nocapture
+@rem @echo #########################################################################
+@rem @echo Running unit tests...
+@rem @echo #########################################################################
+@rem nosetests tests -a "!integration"--nocapture
 
 @rem If tests fail, do NOT build!
-@if %errorlevel% neq 0 exit /b %errorlevel%
+@rem @if %errorlevel% neq 0 exit /b %errorlevel%
 
 @echo #########################################################################
 @echo Running integration tests...
