e5046cbc950c3748eb3da6496d29f6161d88fbfc
Lukasz Taczuk <overfl0@users.noreply.github.com>
2016-03-04 02:06:36 +0100

    Allow skipping tests while building the exe.

--- build.bat
+++ build.bat
@@ -4,6 +4,9 @@
 del tblauncher.exe c:\vagrant\tblauncher 2> NUL
 for /d %%G in ("build\tblauncher\setuptools*.egg") do rmdir /s /q "%%~G"
 
+@rem uncomment below to skip tests
+@rem goto end_of_tests
+
 @echo #########################################################################
 @echo Running unit tests...
 @echo #########################################################################
@@ -23,6 +26,8 @@ nosetests tests -a "integration" --nocapture
 @echo #########################################################################
 @echo Tests passed, building the launcher...
 @echo #########################################################################
+:end_of_tests
+
 python c:\Kivy-1.8.0-py2.7-win32\Python27\Scripts\pyinstaller-script.py tblauncher.spec
 copy dist\tblauncher.exe c:\vagrant
 copy dist\tblauncher.exe .
