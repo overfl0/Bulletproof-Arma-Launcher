30414673f455c242fa541d17364518d22586c3fa
Lukasz Taczuk <overfl0@users.noreply.github.com>
2016-10-14 02:08:48 +0200

    Docstrings for preparer. Moved missing_responses out of the swich statements.

--- src/sync/preparer.py
+++ src/sync/preparer.py
@@ -53,6 +53,8 @@ class MessageHandler(object):
             return callback(params)
 
     def send_message(self, command, params=None):
+        """A wrapper around message_queue.progress to send messages."""
+
         data = {
             'special_message': {
                 'command': command,
@@ -62,6 +64,10 @@ class MessageHandler(object):
         self.message_queue.progress(data)
 
     def bind_message(self, command, callback):
+        """Bind <callback> function to be called when <command> message arrives.
+        Note: there can be only ONE callback assigned to a command at any time.
+        """
+
         self.callbacks[command] = callback
 
 
@@ -77,6 +83,8 @@ class Preparer(object):
         self.message_handler.bind_message('mod_search', self.on_mod_search_message)
 
     def _get_mod_by_foldername(self, foldername):
+        """Helper function to get the mod object by its foldername attribute."""
+
         for mod in self.mods:
             if mod.foldername == foldername:
                 return mod
@@ -84,10 +92,20 @@ class Preparer(object):
         raise KeyError('Could not find mod {}'.format(foldername))
 
     def on_terminate_message(self):
+        """Terminate message has been received."""
+
         Logger.info('TorrentSyncer wants termination')
         self.force_termination = True
 
     def on_mod_reuse_message(self, params):
+        """mod_reuse message has been received.
+        If the argument is copy, copy the directory to the new location.
+        If the argument is use, create a symlink (NTFS Junction currently).
+        Decrease missing_responses.
+        """
+
+        self.missing_responses -= 1
+
         mod_name = params['mod_name']
         mod = self._get_mod_by_foldername(mod_name)
 
@@ -99,7 +117,6 @@ class Preparer(object):
             self.message_handler.message_queue.progress({'msg': 'Creating junction for mod {} finished!'.format(mod_name), 'log': []}, 0)
 
             self.missing_mods.remove(mod)
-            self.missing_responses -= 1
 
         elif params['action'] == 'copy':
             Logger.info('Message: Mod reuse: copy, mod: {}'.format(mod_name))
@@ -109,33 +126,41 @@ class Preparer(object):
             self.message_handler.message_queue.progress({'msg': 'Copying mod {} finished!'.format(mod_name), 'log': []}, 0)
 
             self.missing_mods.remove(mod)
-            self.missing_responses -= 1
 
         elif params['action'] == 'ignore':
             Logger.info('Message: Mod reuse: ignore, mod: {}'.format(mod_name))
-            self.missing_responses -= 1
 
         else:
             raise Exception('Unknown mod_reuse action: {}'.format(params['action']))
 
     def on_mod_search_message(self, params):
+        """mod_search message has been received.
+        The user has decided whether to search for mods in yet another
+        directory or to just download all the remaining mods from the internet.
+        """
+
+        self.missing_responses -= 1
+
         if params['action'] == 'download':
             Logger.info('Message: Mod search: download all')
             self.missing_mods = set()  # Clear the missing mods
-            self.missing_responses = 0  # I think it's optional, actually
 
         elif params['action'] == 'search':
             Logger.info('Message: Mod search: search in {}'.format(params['location']))
-            self.missing_responses -= 1
             self.find_mods_and_ask([params['location']])
 
         else:
             raise Exception('Unknown mod_search action: {}'.format(params['action']))
 
     def reject(self, msg):
+        """Wrapper around message_queue.reject."""
+
         self.message_queue.reject({'msg': msg})
 
     def find_mods_and_ask(self, locations=None):
+        """Find all the potential mods located in <locations> and send the
+        message to present the results to the user so they can make a choice.
+        """
         self.message_queue.progress({'msg': 'Searching for missing mods on disk...',
                                     'log': [],
                                     }, 0)
@@ -151,15 +176,10 @@ class Preparer(object):
                                                 'locations': found_mods[mod_name]
                                                 })
             self.missing_responses += 1
-            '''self.message_queue.progress({'msg': 'Found possible places for {}'.format(mod_name),
-                                        'log': [],
-                                        'mod_found_action': {
-                                            'mod_name': mod_name,
-                                            'locations': found_mods[mod_name]
-                                        }
-                                        }, 0)'''
 
     def request_directory_to_search(self):
+        """Send a message with the list of missing mods to the main process."""
+
         mods_names = [mod.foldername for mod in self.missing_mods]
         self.message_handler.send_message('missing_mods', mods_names)
         self.missing_responses += 1
@@ -209,5 +229,9 @@ class Preparer(object):
 
 
 def prepare_all(message_queue, mods):
+    """Prepare all the mods' directories before downloading content.
+    See preparer.run doc for more info.
+    """
+
     preparer = Preparer(message_queue, mods)
     preparer.run()
