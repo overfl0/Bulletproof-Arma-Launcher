c6f159d4cefbb418be8e2e2bc1fe0141629a88f3
Lukasz Taczuk <overfl0@users.noreply.github.com>
2017-02-13 01:33:37 +0100

    Slimmed down mainwidget.py

--- src/gui/mainwidget.py
+++ src/gui/mainwidget.py
@@ -14,35 +14,9 @@
 from __future__ import unicode_literals
 
 import kivy.app
-import os
-import textwrap
-import time
 
 from kivy.uix.widget import Widget
-from kivy.logger import Logger
 from kivy.clock import Clock
-
-from view.errorpopup import ErrorPopup
-from view.messagebox import MessageBox
-
-from utils.devmode import devmode
-
-
-class TestError(Exception):
-    def __init__(self, msg):
-        self.msg = msg
-
-    def __str__(self):
-        return repr(self.msg)
-
-
-def test_exc_in_process():
-    """function for testing if the exceptions gets rethrown into
-    the kivy main process
-    """
-    pass
-    time.sleep(2)
-    raise TestError('This is a test error')
 
 
 class MainWidget(Widget):
@@ -53,7 +27,6 @@ class MainWidget(Widget):
         super(MainWidget, self).__init__(**kwargs)
         self.controller = Controller(self)
 
-
 class Controller(object):
     def __init__(self, widget):
         super(Controller, self).__init__()
@@ -63,14 +36,5 @@ class Controller(object):
         # this effectively calls on_next_frame, when the view is ready
         Clock.schedule_once(self.on_next_frame, 0)
 
-    def on_testpopupbutton_release(self, btn):
-        return  # Disable this for the alpha release
-        # raise TestError('This is an test error')
-        # self.get_status_image().set_image('attention')
-
     def on_next_frame(self, dt):
         pass
-
-    def get_status_image(self):
-        """retrieve the status image from the tree"""
-        return self.view.ids.main_screen_manager.get_screen('install_screen').ids.status_image
